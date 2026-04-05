5804d5a52169803eb0e0db440815e001f16b0d44
Stijn Vergauwen <stijnvergauwen@gmail.com>
2026-04-05 14:28:43 +0200

    added some wasm specific configuration and fixes

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,50 @@ use avian_first_person_shooter_test::{
     world::WorldPlugin,
 };
 use avian3d::prelude::*;
-use bevy::feathers::{FeathersPlugins, dark_theme::create_dark_theme, theme::UiTheme};
+use bevy::{
+    asset::AssetMetaCheck,
+    feathers::{FeathersPlugins, dark_theme::create_dark_theme, theme::UiTheme},
+};
 use bevy::{prelude::*, window::WindowMode};
 
 const FULLSCREEN: bool = true;
 
 fn main() {
+    let mut default_plugins = DefaultPlugins.build().set(AssetPlugin {
+        // Checking for meta files that don't exist can cause errors on Wasm builds.
+        meta_check: AssetMetaCheck::Never,
+        ..default()
+    });
+
+    #[cfg(target_family = "wasm")]
+    {
+        default_plugins = default_plugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "First-person shooter prototype".to_string(),
+                fit_canvas_to_parent: true,
+                ..default()
+            }),
+            ..default()
+        });
+    }
+
+    #[cfg(not(target_family = "wasm"))]
+    if FULLSCREEN {
+        default_plugins = default_plugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                mode: WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
+                ..default()
+            }),
+            ..default()
+        });
+    };
+
     App::new()
         .add_plugins((
-            if FULLSCREEN {
-                DefaultPlugins.set(WindowPlugin {
-                    primary_window: Some(Window {
-                        mode: WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
-                        ..default()
-                    }),
-                    ..default()
-                })
-            } else {
-                DefaultPlugins.build()
-            },
+            default_plugins,
             PhysicsPlugins::default().with_collision_hooks::<PlayerWeaponCollisionHooks>(),
-            // PhysicsDebugPlugin,
+            #[cfg(feature = "dev")]
+            PhysicsDebugPlugin,
             PhysicsPickingPlugin,
             FeathersPlugins,
             WorldPlugin,
